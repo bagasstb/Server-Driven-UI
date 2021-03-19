7901a44cd84d424ff25f48defdf190e4a1dd13de
Satrio Teleng Bagaskoro <telengbagaskorosatrio@gmail.com>
2021-03-19 12:32:49 +0700

    Initial Commit

---
+++ Server-Driven-UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Server-Driven-UI
+//
+//  Created by bagasstb on 19/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
